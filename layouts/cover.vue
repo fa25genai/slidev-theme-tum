1558382ae2af7ed56335309a20e36ac11decd21f
Kilian Krampf <kilian@krampf.de>
2025-09-23 17:31:36 +0200

    Initial rudimentary TUM layout

---
+++ layouts/cover.vue
@@ -0,0 +1,42 @@
+<script setup>
+const props = defineProps({
+  university: {
+    type: String,
+    default: "Technical University of Munich"
+  },
+  school: {
+    type: String,
+    default: "TUM School of Computation, Information and Technology"
+  },
+  chair: {
+    type: String,
+    default: "Applied Education Technologies"
+  }
+})
+</script>
+
+<template>
+  <div class="slidev-layout cover">
+    <div class="cover-institute">
+      <span>
+        {{ props.chair }}<br/>
+        {{ props.school }}<br/>
+        {{ props.university }}
+      </span>
+    </div>
+    <div class="my-auto w-full">
+      <slot />
+    </div>
+    <img class="tum-uhrenturm" src="../assets/TUM_Uhrenturm.png" alt="TUM Uhrenturm">
+  </div>
+</template>
+
+<style lang="css">
+.cover-institute {
+
+  position: absolute;
+  left: 1.5rem;
+  top: 1.5rem;
+  color: #0065BD;
+}
+</style>
---
+++ layouts/intro.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  university: {
+    type: String,
+  },
+  school: {
+    type: String,
+  },
+  chair: {
+    type: String,
+  }
+})
+</script>
+
+<template>
+  <div class="slidev-layout intro">
+    <div class="my-auto">
+      <slot />
+      <h5>
+        {{ props.chair }}<br/>
+        {{ props.school }}<br/>
+        {{ props.university }}
+      </h5>
+    </div>
+    <img class="tum-uhrenturm" src="../assets/TUM_Uhrenturm.png" alt="TUM Uhrenturm">
+  </div>
+</template>
