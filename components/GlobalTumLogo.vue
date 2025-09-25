1b42798fe133b6fc72d07f2546193cb846742c25
Kilian Krampf <kilian@krampf.de>
2025-09-25 20:48:27 +0200

    Improve TUM logo implementation & image display

---
+++ components/GlobalTumLogo.vue
@@ -0,0 +1,20 @@
+<script setup>
+import tumLogo from "../assets/tum_logo_blue.svg"
+
+</script>
+
+<template>
+  <div class="tum-logo">
+    <img :src="tumLogo" alt="TUM">
+  </div>
+</template>
+
+<style scoped lang="css">
+.tum-logo {
+  position: absolute;
+  right: 2.7%;
+  top: 5.6%;
+  width: 6%;
+  height: auto;
+}
+</style>
